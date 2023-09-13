53c9b6df268d48fb76462ff30d9fa5fd778712b5
Sophie-Ndobe <ndobes76@gmail.com>
2023-09-13 14:05:26 +0200

    Applied states to update city on search

--- src/SearchWeather.js
+++ src/SearchWeather.js
@@ -1,10 +1,25 @@
-import React from "react";
-import "./SearchWeather.css"
+import React, { useState } from "react";
+import "./SearchWeather.css";
 
 export default function SearchWeather() {
+  let [city, setCity] = useState(" ");
+  
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`${city}`);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
   return (
-    <form className="SearchWeather">
-      <input type="search" placeholder="Enter a city name..."></input>
+    <form className="SearchWeather" onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city name..."
+        onChange={updateCity}
+      ></input>
       <input type="submit" value="Search"></input>
     </form>
   );

7f590554bb2456d00136502bcdf2fc002b74f76f
Sophie-Ndobe <ndobes76@gmail.com>
2023-09-13 19:39:24 +0200

    Updated the SearchWeather component
    -Included axios and started enabling the api call to be made
    -Started adding JSX content building the app
    -Updated the App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import SearchWeather from "./SearchWeather";
 
 function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
       <SearchWeather />
     </div>
   );
--- src/SearchWeather.js
+++ src/SearchWeather.js
@@ -1,26 +1,54 @@
 import React, { useState } from "react";
 import "./SearchWeather.css";
+import axios from "axios";
 
 export default function SearchWeather() {
-const [city, setCity] = useState(" ");
+  const [city, setCity] = useState(" ");
+  const [weather, setWeather] = useState(" ");
 
+  function displayWeather(response) {
+    console.log(response);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`${city}`);
+    let apiKey = "8342a5044534040e24d2802ce4fcc6ac";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
   }
 
   function updateCity(event) {
     setCity(event.target.value);
   }
   return (
-    <form className="SearchWeather" onSubmit={handleSubmit}>
-      <input
-        type="search"
-        placeholder="Enter a city name..."
-        onChange={updateCity}
-      ></input>
-      <input type="submit" value="Search"></input>
-    </form>
+    <div className="SearchWeather">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a city name..."
+          onChange={updateCity}
+        ></input>
+        <input type="submit" value="Search"></input>
+      </form>
+      <h2>Paris</h2>
+      <ul>
+        <li>Wednesday 13:53</li>
+        <li>Clouds</li>
+      </ul>
+
+      <div className="row">
+        <span className="col-6">
+          <img src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png" />
+          <h1>25</h1>
+        </span>
+        <span className="col-6">
+          <ul>
+            <li>Precipitation: 60%</li>
+            <li>Wind: 4km/h</li>
+          </ul>
+        </span>
+
+      </div>
+    </div>
   );
 }
